4cefbc1a7360c27380e15e317c617781e0eb42c2
Kewelin K <kewelin.313@gmail.com>
2020-09-16 15:49:31 +0700

    quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,15 @@
+//1
+let word = 'Hello my name is Joke'
+console.log(word.length)
+//2
+var str = "Hello my name is Joke"
+var pos = str.search("Joke");
+console.log(pos)
+//3
+// const word_3 = "Well open up your mind and see like me"
+// const r = word_3.split(' ')
+// r.forEach(e => e === 'open' ? console.log(e) : '')
+
+var str1 = "Well open up your mind and see like me"
+var pos = str1.slice(5, 9);
+console.log(pos)
