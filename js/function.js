c6d9f1a22200f25ebf834393a9f6a976c8d345ed
Kewelin K <kewelin.313@gmail.com>
2020-09-16 14:05:51 +0700

    {add} function file

---
+++ js/function.js
@@ -0,0 +1,14 @@
+// function add(a, b){
+//     return a + b
+// }
+
+// function mul(a, b){
+//     return a * b
+// }
+
+// console.log(add(2, 3))
+
+const add = (a, b) => a + b
+
+console.log(add(2, 3))
+
