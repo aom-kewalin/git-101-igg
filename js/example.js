68898fad4848fae5df78575e0b1749893c8173aa
Kewelin K <kewelin.313@gmail.com>
2020-09-16 13:39:00 +0700

    add code

---
+++ js/example.js
@@ -0,0 +1,27 @@
+console.log("Hello World")
+
+const PI = 3.14
+console.log("PI:", PI)
+
+let name ="Jhon"
+name = "Jame"
+console.log("name:", name)
+
+let  test ="Hello"
+name = `${test} Jame`
+console.log("name:", name)
+
+let arr = ['apple','banana','kiwi']
+console.log("arr:", arr[2])
+
+let person ={
+    name: 'JOKE',
+    age: 12,
+    gender: {
+        name: 'male'
+    },
+    arr
+}
+console.log(person.name)
+console.log(person.age)
+console.log(person.gender)
